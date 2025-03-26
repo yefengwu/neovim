4e8fc0f1cd9b5c4cc32bce1afc1c9a728ff6b985
frank <franklxw@outlook.com>
2025-03-26 14:35:44 +0800

    fix: fix notify background color warning

--- lua/winplugins/which-key.lua
+++ lua/winplugins/which-key.lua
@@ -12,11 +12,17 @@ return {
 		-- your configuration comes here
 		-- or leave it empty to use the default settings
 		-- refer to the configuration section below
+		icons = {
+			mappings = false,
+		},
+		triggers = {
+			{ "<auto>", mode = "nxso" },
+		},
 	},
 	keys = {
-		{ "<A-p>", "<cmd>Telescope commands<CR>", desc = "Commands"},
-		{ "<Leader>f", "<cmd>Telescope find_files<CR>", desc = "Find Files"},
-		{ "<Leader>p", "<cmd>Telescope project<CR>", desc = "Project" },
+		{ "<A-p>", "<cmd>Telescope commands<CR>", desc = "List Commands" },
+		{ "<Leader>f", "<cmd>Telescope find_files<CR>", desc = "Find Files" },
+		{ "<Leader>p", "<cmd>Telescope project<CR>", desc = "List Project" },
 		{ "<Leader>s", "<cmd>Telescope live_grep<CR>", desc = "Live Grep" },
 	},
 }
