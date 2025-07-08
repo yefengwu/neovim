870d41d807fb9cdaa5590cad86cb7a6377c46a0d
xuwei <franklxw@outlook.com>
2025-07-08 15:25:16 +0800

    fi: fix toggleterm

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -3,50 +3,28 @@ return {
 	config = function()
 		local os = vim.fn.has("win32") == 1 and "windows" or "linux"
 		require("toggleterm").setup({
-			-- size can be a number or function which is passed the current terminal
-			size = function(term)
-				if term.direction == "horizontal" then
-					return 15
-				elseif term.direction == "vertical" then
-					return vim.o.columns * 0.4
-				end
-			end,
+			direction = "float",
 			open_mapping = [[<c-\>]],
-			hide_numbers = true, -- hide the number column in toggleterm buffers
-			shade_filetypes = {},
-			shade_terminals = false,
-			-- shading_factor = "<number>", -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
-			start_in_insert = true,
-			insert_mappings = true, -- whether or not the open mapping applies in insert mode
-			terminal_mappings = true, -- whether or not the open mapping applies in the opened terminals
-			persist_size = true,
-			direction = "float", --'vertical' | 'horizontal' | 'window' | 'float',
-			close_on_exit = true, -- close the terminal window when the process exits
-			--[[ shell = vim.o.shell, -- change the default shell ]]
-			shell = os == "windows" and "nu" or "zsh", -- change the default shell
-			-- This field is only relevant if direction is set to 'float'
+             --[[ on_create = fun(t: Terminal), ]]
+            autochdir = false,
 			float_opts = {
-				-- The border key is *almost* the same as 'nvim_open_win'
-				-- see :h nvim_open_win for details on borders however
-				-- the 'curved' border is a custom border type
-				-- not natively supported but implemented in this plugin.
-				border = "curved", --'single' | 'double' | 'shadow' | 'curved' | ... other options supported by win open
-				width = 80,
-				height = 30,
+				border = "curved",
+				-- like `size`, width, height, row, and col can be a number or function which is passed the current terminal
+				--[[ width = <value>, ]]
+				--[[ height = <value>, ]]
+				--[[ row = <value>, ]]
+				--[[ col = <value>, ]]
 				winblend = 3,
-				highlights = {
-					border = "Normal",
-					background = "Normal",
-				},
+				--[[ zindex = <value>, ]]
+				title_pos = "left",
 			},
 			winbar = {
-				enabled = true,
+				enabled = false,
 				name_formatter = function(term) --  term: Terminal
 					return term.name
 				end,
 			},
 		})
-
 		function _G.set_terminal_keymaps()
 			local opts = { noremap = true }
 			vim.api.nvim_buf_set_keymap(0, "t", "<esc>", [[<C-\><C-n>]], opts)
